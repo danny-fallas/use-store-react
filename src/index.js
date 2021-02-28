2c2613219f1d79228581ba8fa89b9b303ed693a5
Danny Fallas <dann_1990@hotmail.com>
2021-02-28 00:11:22 -0600

    Improved code readability

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
+// Imports
 import { useState, useEffect } from 'react';
 
+// Constants
+const ssrStateMock = [false, () => false];
+
+// Private functions
 const isBrowser = (typeof window !== 'undefined');
 const refresh = (callback) => setInterval(callback, 500);
-
+const set = (storage, key, value) => storage.setItem(key, btoa(JSON.stringify(value || null)));
 const get = (storage, key) => {
   const raw = storage.getItem(key);
   return (raw && raw.length) ? JSON.parse(atob(raw)) : null;
 };
 
-const set = (storage, key, value) => storage.setItem(key, btoa(JSON.stringify(value || null)));
-
+// Public functions
 const usePersistedState = (key, defaultValue = false, isNew = false) => {
-  if (!isBrowser) return [false, () => false];
+  if (!isBrowser) return ssrStateMock;
 
   const storedValue = get(localStorage, key);
-  const [state, setState] = useState(() => !isNew ? (storedValue !== null) ? storedValue : defaultValue : defaultValue);
+  const state = useState(() => !isNew ? (storedValue !== null) ? storedValue : defaultValue : defaultValue);
 
   useEffect(() => { set(localStorage, key, state) }, [key, state]);
 
@@ -23,20 +27,21 @@ const usePersistedState = (key, defaultValue = false, isNew = false) => {
     return () => clearInterval(interval);
   }, []);
 
-  return [state, setState];
+  return state;
 };
 
 const useSessionState = (key, defaultValue = false, isNew = false) => {
-  if (!isBrowser) return [false, () => false];
+  if (!isBrowser) return ssrStateMock;
 
   const storedValue = get(sessionStorage, key);
-  const [state, setState] = useState(() => !isNew ? (storedValue !== null) ? storedValue : defaultValue : defaultValue);
+  const state = useState(() => !isNew ? (storedValue !== null) ? storedValue : defaultValue : defaultValue);
 
   useEffect(() => { set(sessionStorage, key, state) }, [key, state]);
 
-  return [state, setState];
+  return state;
 };
 
+// Exports
 export {
   usePersistedState,
   useSessionState
