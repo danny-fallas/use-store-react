8442613d3ea76963934679429564d8a9efebe78c
Danny Fallas <dann_1990@hotmail.com>
2021-02-27 23:58:10 -0600

    BUG FIX: Issues when building for production apps (Gatsby, NextJS) with SSR Avoid setting the usePersistedState value to null automatically.

--- src/index.js
+++ src/index.js
@@ -4,18 +4,19 @@ const isBrowser = (typeof window !== 'undefined');
 const refresh = (callback) => setInterval(callback, 500);
 
 const get = (storage, key) => {
-  const raw = isBrowser && storage.getItem(key);
+  const raw = storage.getItem(key);
   return (raw && raw.length) ? JSON.parse(atob(raw)) : null;
 };
 
-const set = (storage, key, value) => isBrowser && storage.setItem(key, btoa(JSON.stringify(value || null)));
+const set = (storage, key, value) => storage.setItem(key, btoa(JSON.stringify(value || null)));
 
 const usePersistedState = (key, defaultValue = false, isNew = false) => {
+  if (!isBrowser) return;
+
   const storedValue = get(localStorage, key);
   const [state, setState] = useState(() => !isNew ? (storedValue !== null) ? storedValue : defaultValue : defaultValue);
 
   useEffect(() => { set(localStorage, key, state) }, [key, state]);
-  useEffect(() => { !state && setState(null) }, [state]);
 
   useEffect(() => {
     const interval = refresh(() => setState(get(localStorage, key)));
@@ -26,11 +27,12 @@ const usePersistedState = (key, defaultValue = false, isNew = false) => {
 };
 
 const useSessionState = (key, defaultValue = false, isNew = false) => {
+  if (!isBrowser) return;
+
   const storedValue = get(sessionStorage, key);
   const [state, setState] = useState(() => !isNew ? (storedValue !== null) ? storedValue : defaultValue : defaultValue);
 
   useEffect(() => { set(sessionStorage, key, state) }, [key, state]);
-  useEffect(() => { !state && setState(null) }, [state]);
 
   return [state, setState];
 };
